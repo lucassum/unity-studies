6157e263071f787a5c502c157690a1fddacc2c58
Lucas Tavares <objective.lucas@rdstation-ext.com>
2023-05-24 20:18:05 -0300

    challenge 2 - bonus 2

--- Unit 2 - Basic Gameplay/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Unit 2 - Basic Gameplay/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float cooldown = 0;
 
     // Update is called once per frame
     void Update()
     {
+        if (cooldown > 0)
+            cooldown -= 1 * Time.deltaTime;
+        
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 2;
         }
     }
 }
