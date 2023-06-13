b82b7cd4bf92de6f4c92e8e1addf0dfa05566d3f
Lucas Tavares <lucasjordandt@gmail.com>
2023-06-13 13:02:17 -0300

    Unit 3.2 - step 5

--- Unit 3/Assets/Scripts/MoveLeft.cs
+++ Unit 3/Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 30f;
+    private PlayerController playerController;
     // Start is called before the first frame update
     void Start()
     {
-
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (!playerController.gameOver)
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 }
