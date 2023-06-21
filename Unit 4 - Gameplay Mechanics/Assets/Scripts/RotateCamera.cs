e4a16820e32f8086d3e702a973b6c07695d417c6
Lucas Tavares <lucasjordandt@gmail.com>
2023-06-21 13:54:05 -0300

    Unit 4.1 - step 4

--- Unit 4 - Gameplay Mechanics/Assets/Scripts/RotateCamera.cs
+++ Unit 4 - Gameplay Mechanics/Assets/Scripts/RotateCamera.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    public float rotationSpeed = 20f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+
+
     }
 }
