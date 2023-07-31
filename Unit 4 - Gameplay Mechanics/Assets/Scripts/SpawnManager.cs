6f96de791b7afc818c98843b4a5a0d8badf01552
Lucas Tavares <lucasjordandt@gmail.com>
2023-07-31 14:00:36 -0300

    Unit 4.2 - step 5

--- Unit 4 - Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
+++ Unit 4 - Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject prefab;
+    private float spawnRange = 9;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(prefab, new Vector3(0, 0, 6), prefab.transform.rotation);
+        float spawnX = Random.Range(-spawnRange, spawnRange);
+        float spawnZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 spawnPos = new Vector3(spawnX, 0, spawnZ);
+        Instantiate(prefab, spawnPos, prefab.transform.rotation);
     }
 
     // Update is called once per frame
